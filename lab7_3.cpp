bec6ed7cd2023f59140a9a03bdd69f50246a1383
Nunnalin Bingbing <61963924+snnmbb@users.noreply.github.com>
2020-12-29 16:25:46 +0700

    edit code

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -9,3 +9,84 @@
 "ONE-NIGHT-STAND"
 "MARRIED"
 */
+
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int age;
+    double height;
+    string status;
+    double property;
+    
+    cout<<"Enter your age: ";
+    cin>>age;
+    
+    
+    if(age<=20)
+    {
+        cout<<"Enter your height: ";
+        cin>>height;
+
+        if(height<160&&height<175)
+        {
+            cout<<"Your status = ";
+            cout<<"UNFRIEND";
+
+        }
+
+        else 
+        {
+            if(height<175)
+            {
+                cout<<"Your status = ";
+                cout<<"FRIEND";
+
+            }
+            else
+            {
+                cout<<"Enter your property: ";
+                cin>> property;
+                if(property>69000000)
+                {
+                    cout<<"Your status = ";
+                    cout<<"MARRIED";
+                }
+                else
+                {
+                    cout<<"Your status = ";
+                    cout<<"ONE-NIGHT-STAND";
+                }
+            }
+        } 
+
+
+    }
+    else
+    {
+        if(age<30)
+        {
+            cout<<"Enter your property: ";
+            cin>> property;
+            if(property>10000000)
+            {
+                cout<<"Your status = ";
+                cout<<"BEST FRIEND";
+            }
+            else
+            {
+                cout<<"Your status = ";
+                cout<<"UNFRIEND";
+            }
+        }
+        else
+        {
+            cout<<"Your status = ";
+            cout<<"UNFRIEND";
+        }
+        
+    }
+}
